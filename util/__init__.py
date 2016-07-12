66db24731bdff24c0f632c5080998e70f0435a00
Jon Gauthier <jon@gauthiers.net>
2016-07-11 17:10:46 -0700

    stage floaty_ops shared object (custom TF ops). Sort of hacky?

--- util/__init__.py
+++ util/__init__.py
@@ -3,5 +3,7 @@ from util.data import *
 
 
 # Load custom TF module.
+import os
 import tensorflow as tf
-floaty = tf.load_op_library("floaty_ops.so")
+floaty_path = os.path.join(os.path.dirname(__file__), "..", "lib", "floaty_ops.so")
+floaty = tf.load_op_library(floaty_path)
